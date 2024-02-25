79ff785bda14422d103f7f67cce5381b704ee94e
catandA <75292299+catandA@users.noreply.github.com>
2024-02-25 19:18:04 +0800

    给Status添加职业与护甲字段

--- src/main/java/me/catand/spdnetserver/data/Status.java
+++ src/main/java/me/catand/spdnetserver/data/Status.java
@@ -13,4 +13,12 @@ public class Status {
 	private int challenges;
 	private long seed;
 	private int depth;
+	private int heroClass;
+	/**
+	 * 护甲等阶, 大多数情况下就是Armor的tier
+	 * 但是有两种特殊情况:
+	 * 没穿护甲的时候是0
+	 * 穿职业护甲的时候是10(和板甲区分开)
+	 */
+	private int armorTier;
 }
